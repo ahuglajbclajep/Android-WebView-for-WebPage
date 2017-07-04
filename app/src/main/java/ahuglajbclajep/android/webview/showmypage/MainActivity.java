2ba758874c4ddd135893f6c6f441128c10ced099
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-07-05 01:00:10 +0900

    upd can goback pages

--- app/src/main/java/ahuglajbclajep/android/webview/showmypage/MainActivity.java
+++ app/src/main/java/ahuglajbclajep/android/webview/showmypage/MainActivity.java
@@ -2,19 +2,21 @@ package ahuglajbclajep.android.webview.showmypage;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView webView = (WebView) findViewById(R.id.webview);
+        webView = (WebView) findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebChromeClient() {
             @Override
@@ -25,5 +27,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         webView.loadUrl("https://ajax-qrcode-springboot.herokuapp.com");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
