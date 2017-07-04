dfb5095e9f34baabd331d7c6e7bbad3890316503
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-07-05 00:56:07 +0900

    upd show js-alert as toast

--- app/src/main/java/ahuglajbclajep/android/webview/showmypage/MainActivity.java
+++ app/src/main/java/ahuglajbclajep/android/webview/showmypage/MainActivity.java
@@ -2,7 +2,10 @@ package ahuglajbclajep.android.webview.showmypage;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
 
         WebView webView = (WebView) findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                result.cancel();
+                return true;
+            }
+        });
         webView.loadUrl("https://ajax-qrcode-springboot.herokuapp.com");
     }
 }
