a68f110a0d6b205b813dfda35a7271f7e5428969
ahuglajbclajep <ahuglajbclajep@gmail.com>
2017-07-11 16:09:00 +0900

    upd onKeyDown to onBackPressed

--- app/src/main/java/ahuglajbclajep/android/webview/showmypage/MainActivity.java
+++ app/src/main/java/ahuglajbclajep/android/webview/showmypage/MainActivity.java
@@ -47,11 +47,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
-            webView.goBack();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
+    public void onBackPressed() {
+        webView.goBack();
     }
 }
